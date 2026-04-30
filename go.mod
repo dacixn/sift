135b59dfb441b29f98effb84ea9ab5b476286d80
Dacian <contact@dacian.co>
2026-04-30 13:13:29 -0400

    Replace yaml with toml

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/dacixn/sift
 go 1.26.2
 
 require github.com/goccy/go-yaml v1.19.2
+
+require github.com/BurntSushi/toml v1.6.0 // indirect
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,18 +1,16 @@
 package config
 
 import (
-	"fmt"
 	"os"
-	"path/filepath"
 
-	"github.com/goccy/go-yaml"
+	"github.com/BurntSushi/toml"
 )
 
 type Config struct {
-	Groups            map[string][]string `yaml:"groups"`
-	EnableGroups      bool                `yaml:"enableGroups"`
-	DirPrefix         string              `yaml:"dirPrefix"`
-	DefaultWorkingDir string              `yaml:"workingDir"`
+	Groups            map[string][]string `toml:"groups"`
+	EnableGroups      bool                `toml:"enableGroups"`
+	DirPrefix         string              `toml:"dirPrefix"`
+	DefaultWorkingDir string              `toml:"workingDir"`
 }
 
 // put yaml in here
@@ -37,11 +35,11 @@ func InitConfig() *Config {
 
 func readConfig(path string) (*Config, error) {
 	var cfg Config
-	yamlBytes, err := os.ReadFile(path)
+	tomlBytes, err := os.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
-	err = yaml.Unmarshal(yamlBytes, &cfg)
+	err = toml.Unmarshal(tomlBytes, &cfg)
 	if err != nil {
 		return nil, err
 	}
@@ -49,39 +47,10 @@ func readConfig(path string) (*Config, error) {
 }
 
 func locateConfigFile() (string, error) {
-	// search user config dir for ./sift/config.yaml
-	// otherwise search working dir for config.yaml
-	// WTF AM I DOING
 	// hardcoded for now, change with flag eventually (probably with app struct)
+	configName := "config.toml"
 
-	var configDir string
-	var configPath string
-	var configName string = "config.yaml"
-
-	// might be unidiomatic but what if i flip the conditions
-	// if err == nil, else is easier to read
-
-	configDir, err := os.UserConfigDir()
-	if err != nil {
-		configDir, err = os.Getwd()
-		if err != nil {
-			return "", fmt.Errorf("Error opening directories: %w", err)
-		} else {
-			configPath = filepath.Join(configDir, configName)
-			_, err = os.Stat(configPath)
-		}
-	} else {
-		configDir = filepath.Join(configDir, "sift") // only add sift subfolder if in .config/
-		configPath = filepath.Join(configDir, configName)
-		_, err := os.Stat(configPath)
-		if err != nil {
-			return "", fmt.Errorf("Error locating configuration file: %w", err)
-		}
-	}
-	_, err = os.Stat(configPath)
-	if err != nil {
-		return "", fmt.Errorf("Error locating configuration file: %w", err)
-	}
+	// ...
 
 	return configPath, nil
 }
