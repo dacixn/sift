6eb5b470b006e6529f0dc1008ed22ea1146af6a1
Dacian <contact@dacian.co>
2026-04-30 12:15:40 -0400

    Added terrible logic for config
    this doesn't even work
    
    rewrite OVERDUE asap

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/goccy/go-yaml"
 )
@@ -13,7 +15,27 @@ type Config struct {
 	DefaultWorkingDir string              `yaml:"workingDir"`
 }
 
-func ReadConfig(path string) (*Config, error) {
+// put yaml in here
+// var configFile []byte
+
+func InitConfig() *Config {
+	// locateConfig
+	// if not exist plantConfig
+	// then readConfig
+	// wtf should this return??
+	// take no args return config ptr
+	path, err := locateConfigFile()
+	if err != nil {
+		path, err = plantConfigFile()
+	}
+
+	cfg, err := readConfig(path)
+	return cfg
+}
+
+// functions local to this module
+
+func readConfig(path string) (*Config, error) {
 	var cfg Config
 	yamlBytes, err := os.ReadFile(path)
 	if err != nil {
@@ -24,4 +46,46 @@ func ReadConfig(path string) (*Config, error) {
 		return nil, err
 	}
 	return &cfg, nil
+}
+
+func locateConfigFile() (string, error) {
+	// search user config dir for ./sift/config.yaml
+	// otherwise search working dir for config.yaml
+	// WTF AM I DOING
+	// hardcoded for now, change with flag eventually (probably with app struct)
+
+	var configDir string
+	var configPath string
+	var configName string = "config.yaml"
+
+	// might be unidiomatic but what if i flip the conditions
+	// if err == nil, else is easier to read
+
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		configDir, err = os.Getwd()
+		if err != nil {
+			return "", fmt.Errorf("Error opening directories: %w", err)
+		} else {
+			configPath = filepath.Join(configDir, configName)
+			_, err = os.Stat(configPath)
+		}
+	} else {
+		configDir = filepath.Join(configDir, "sift") // only add sift subfolder if in .config/
+		configPath = filepath.Join(configDir, configName)
+		_, err := os.Stat(configPath)
+		if err != nil {
+			return "", fmt.Errorf("Error locating configuration file: %w", err)
+		}
+	}
+	_, err = os.Stat(configPath)
+	if err != nil {
+		return "", fmt.Errorf("Error locating configuration file: %w", err)
+	}
+
+	return configPath, nil
+}
+
+func plantConfigFile() (path string, err error) {
+	return path, nil
 }
